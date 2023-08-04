ee7aba3d11808e0ae5f3d0ad9d0218dae0a26c33
fredeprez <deprez.frederik@gmail.com>
2023-08-04 22:27:53 +0200

    Add day2 part 1

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@ mod days;
 use std::str::FromStr;
 
 use anyhow::{anyhow, Error, Result};
-use days::{day::Day, day1::Day1};
+use days::{day::Day, day1::Day1, day2::Day2};
 
 enum Days {
     Day1(Day1),
+    Day2(Day2),
 }
 
 impl FromStr for Days {
@@ -15,6 +16,7 @@ impl FromStr for Days {
     fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
         match s {
             "day1" => return Ok(Days::Day1(Day1 {})),
+            "day2" => return Ok(Days::Day2(Day2 {})),
             _ => return Err(anyhow!("Wrong argument")),
         }
     }
@@ -24,6 +26,7 @@ impl Day for Days {
     fn run(&self) {
         match self {
             Days::Day1(day1) => day1.run(),
+            Days::Day2(day2) => day2.run(),
         }
     }
 }
